fcffa6b2e2b2189e564260c243be41d8616c2694
byAd12 <92215714+byAd12@users.noreply.github.com>
2024-07-22 18:13:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from cx_Freeze import setup, Executable
 
 ejecutable = Executable(script="app.pyw", base=None)
-setup(name="Multi_App", version="1.0", description="Aplicación con múltiples funciones", author="byAd12",
+setup(name="Multi_App", version="1.0", description="...", author="byAd12",
     options={"build_exe": {"include_files": ["func/"]}},
     executables=[ejecutable]
 )
